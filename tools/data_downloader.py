adbf47fabf6bf5954c6f98bf2cdee182b2a5e347
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-14 04:11:47 +0000

    Adding tarfile member sanitization to extractall()

--- tools/data_downloader.py
+++ tools/data_downloader.py
@@ -189,7 +189,26 @@ def unpack_dataset(path):
         if item.endswith('tar.gz') or item.endswith('tgz'):
             print(f'Extracting {item}')
             with tarfile.open(os.path.join(path, item)) as tar:
-                tar.extractall(os.path.join(destfolder, item[:item.find('.')]))
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, os.path.join(destfolder,item[:item.find(".")]))
         elif item.endswith('gz'):
             with gzip.open(os.path.join(path, item)) as gz:
                 with open(os.path.join(destfolder, item[:item.find('.')]),
